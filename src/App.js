87340becd8f8e53faec8f7b60a19b614f86a4ba2
Nick Balestra-Foster <nick@balestrafoster.com>
2022-06-26 13:20:14 -0700

    updates

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import "./App.css";
 import { useState } from "react";
 import { ethers } from "ethers";
 import Greeter from "./artifacts/contracts/Greeter.sol/Greeter.json";
-import Token from "./artifacts/contracts/Token.sol/Token.json";
+import Token from "./artifacts/contracts/Token.sol/NBXToken.json";
 
-const greeterAddress = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
-const tokenAddress = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9";
+const greeterAddress = "0x9A676e781A523b5d0C0e43731313A708CB607508";
+const tokenAddress = "0x0B306BF915C4d645ff596e518fAf3F9669b97016";
 
 function App() {
   const [greeting, setGreetingValue] = useState();
